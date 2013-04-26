dfde9424bf837100a0622784bbf70d96c3998cd1
Andrés Caro <andres.caro.q@gmail.com>
2013-04-26 08:37:46 -0300

    Código hermoso agregado

---
+++ codigo.py
@@ -0,0 +1,7 @@
+CONSTANTE_RANDOM = 3
+
+def funcion_de_prueba():
+    nombre_usuario = input("Ingrese su nombre, por favor: ")
+    print(nombre_usuario, ", la constante es:", CONSTANTE_RANDOM)
+
+funcion_de_prueba()
